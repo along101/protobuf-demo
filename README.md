04ef8ef779a86ab437c217a05a90a5bbd5809da7
yinzuolong <yinzuolong@ppdai.com>
2017-11-29 16:03:30 +0800

    protobuf demo

---
+++ README.md
@@ -0,0 +1,3 @@
+# protobuf demo
+运行mvn clean compile 会在target/generated-sources/protobuf目录下生成java代码  
+运行测试类TestProtobuff
---
+++ src/test/java/test/TestProtobuff.java
@@ -0,0 +1,26 @@
+package test;
+
+import com.example.tutorial.AddressBookProtos;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by yinzuolong on 2017/11/29.
+ */
+public class TestProtobuff {
+    @Test
+    public void testProto() throws Exception {
+
+        AddressBookProtos.Person person = AddressBookProtos.Person.newBuilder()
+                .setId(100).setName("zhuliangliang").setEmail("zhuliangliang.me@gmail.com").build();
+        System.out.println(person);
+        Assert.assertEquals(100, person.getId());
+        System.out.println(person.toByteString());
+
+        byte[] b = person.toByteArray();
+
+        AddressBookProtos.Person p = AddressBookProtos.Person.parseFrom(b);
+        System.out.println(p.getEmail());
+
+    }
+}
